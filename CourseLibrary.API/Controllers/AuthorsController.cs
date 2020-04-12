c4c3cbdd8b54b13a1ba3f6f7229149cdd33efec2
TuanChauMinh <TuanChauMinh>
2020-04-12 23:13:05 +0700

    Add project files.

---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Helpers;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
+        {
+            _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+
+        }
+        [HttpGet()]
+        public ActionResult<IEnumerable<AuthorDto>> GetAuthors()
+        {
+            throw new Exception("Test exception");
+            //var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            //var authors = new List<AuthorDto>();
+
+            //return Ok(_mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo));
+        }
+
+        [HttpGet("{authorId:guid}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+
+            if(!_courseLibraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+            return Ok  (authorFromRepo);
+
+          
+        }
+    }
+}
